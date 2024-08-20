964c5d9607cadd3ad810db4688ffebaeff219c68
Shara Alencar <155564948+sharalencar@users.noreply.github.com>
2024-08-19 21:28:20 -0300

    Projeto concluído
    Construindo um Sistema de Hospedagem de um Hotel no C#

---
+++ Models/Reserva.cs
@@ -0,0 +1,63 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            int quantidadeDeHospedes = hospedes.Count();
+            if (Suite.Capacidade >= quantidadeDeHospedes)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // *IMPLEMENTE AQUI*
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+            int quantidadeDeHospedes = Hospedes.Count();
+            return quantidadeDeHospedes;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.90m;
+            }
+            return valor;
+        }
+    }
+}
